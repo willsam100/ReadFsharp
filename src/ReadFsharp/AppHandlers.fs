a7d6db8602cfd2de5c9bd66d4500f7a343eba3bd
Sam Williams <>
2019-11-23 10:28:20 +1300

    Late init

---
+++ src/ReadFsharp/AppHandlers.fs
@@ -0,0 +1,68 @@
+﻿module AppHandlers
+
+open System
+open Microsoft.Extensions.Logging
+open Giraffe
+open Microsoft.AspNetCore.Http
+open System.Threading.Tasks
+
+type Resp = {
+    Response: string[]
+}
+
+[<CLIMutable>]
+type ConvertRequest = {
+    CodeBlocks: string[]
+}
+
+let cors  : HttpHandler =
+    setHttpHeader "Access-Control-Allow-Origin" "*"
+    >=> setHttpHeader "Access-Control-Allow-Credentials" "true"
+
+let parsingError (err : string) = RequestErrors.BAD_REQUEST err
+
+let processCode (codeBlocks:string[]) = 
+
+    printfn "%s" codeBlocks.[0]
+
+    let results = 
+        codeBlocks
+        |> Array.map (fun csharp -> 
+                try
+                    FSharper.Core.Converter.runWithConfig false csharp
+                with e -> e.Message )
+    { Response = results}
+            
+let indexHandler  =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        text "Serverless Giraffe Web API" next ctx
+
+let notFound  (next : HttpFunc) (ctx : HttpContext) =
+    let msg = sprintf "Not found: (%s) (%s) (%s)" (ctx.GetRequestUrl()) (ctx.Request.Path.Value) (ctx.Request.PathBase.Value)
+    (setStatusCode 404 >=> text msg) next ctx
+
+
+let webApp: HttpHandler =  
+    subRoute "/proxy+"
+       (choose [
+            GET >=>
+                choose [
+                    routex "/ping" >=> cors >=> json ({Response = [|"pong" |] })
+                    routex "/api" >=> cors >=> indexHandler
+                    routex "/" >=> cors >=> indexHandler 
+                ]
+            POST >=> 
+                choose [
+                    route "/csharp" >=> cors >=> bindJson<ConvertRequest> (fun car -> car.CodeBlocks |> processCode |> json) 
+                ]
+            notFound
+            ])
+        
+//------------------------------
+// Error handler
+// ---------------------------------
+let errorHandler (ex : Exception) (logger : ILogger) =
+    logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
+    clearResponse >=> setStatusCode 500 >=> text ex.Message
+    
+    
---
+++ test/ReadFsharp.Tests/HttpHandlersTests.fs
@@ -0,0 +1,36 @@
+﻿namespace ReadFsharp.Tests
+
+
+open Xunit
+open Amazon.Lambda.TestUtilities
+open Amazon.Lambda.APIGatewayEvents
+
+open System.IO
+open Newtonsoft.Json
+
+open Setup
+
+
+module HttpHandlersTests =
+
+    [<Fact>]
+    let ``Request HTTP Get at /ping``() = async {
+        let request = APIGatewayProxyRequest(Path = "/proxy+/ping", HttpMethod = "GET")
+        let! response = LambdaEntryPoint().FunctionHandlerAsync(request, TestLambdaContext()) |> Async.AwaitTask
+        Assert.Equal(200, response.StatusCode)
+    }
+
+    [<Fact>]
+    let ``Request HTTP Get at /csharp``() = async {
+
+        let csharp = 
+            """{"CodeBlocks":["public class Foo { public int Foo = 42;}"]}"""        
+        let expectedFsharp = """{"response":["type Foo() =\n    member this.Foo = 42"]}"""
+
+        let request = APIGatewayProxyRequest(Path = "/proxy+/csharp", HttpMethod = "POST", Body = csharp)
+        let! response = LambdaEntryPoint().FunctionHandlerAsync(request, TestLambdaContext()) |> Async.AwaitTask
+        Assert.Equal(expectedFsharp, response.Body)
+    }
+
+    [<EntryPoint>]
+    let main _ = 0
